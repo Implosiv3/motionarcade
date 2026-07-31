6057543b968f14b42c735736fd11c1850b058356
danialcala94 <danielalcalavalera@gmail.com>
2026-08-01 07:07:29 +0800

    Canvas view adapted to fit the screen (but export is not working well). New version.

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,12 +1,24 @@
 import "./Canvas.scss";
+
 import { useRef } from "react";
-import { useCanvasStore } from "../../store/canvasStore";
+
+import {
+    useCanvasStore
+} from "../../store/canvasStore";
+
 import Canvas2D from "./Canvas2D";
+
 import CanvasControls from "./CanvasControls/CanvasControls";
 import AnimationControls from "./CanvasControls/AnimationControls/AnimationControls";
 import DownloadControls from "./CanvasControls/DownloadControls/DownloadControls";
-import { useHtmlToPng2d } from "../../features/export/hooks/useHtmlToPng2d";
-import type { SceneData } from "../../features/animation/engine/scene/sceneTypes";
+
+import {
+    useHtmlToPng2d
+} from "../../features/export/hooks/useHtmlToPng2d";
+
+import type {
+    SceneData
+} from "../../features/animation/engine/scene/sceneTypes";
 
 
 type CanvasProps = {
@@ -17,55 +29,77 @@ type CanvasProps = {
 export default function Canvas({
     scene
 }:CanvasProps){
-    // const aspectRatio = useCanvasStore(state => state.canvas.aspectRatio);
-    const mode = useCanvasStore(state => state.canvas.mode);
-    const exportQuality = useCanvasStore(state => state.canvas.exportQuality);
-    const ref = useRef<HTMLDivElement>(null);
 
-    const scale = 0.5;
+    const mode =
+        useCanvasStore(
+            state => state.canvas.mode
+        );
+
+    const exportQuality =
+        useCanvasStore(
+            state => state.canvas.exportQuality
+        );
+
+    const ref =
+        useRef<HTMLDivElement>(null);
+
+    const previewWidth = 960;
+    const previewHeight = 540;
+
+    const scale =
+        previewWidth /
+        scene.width;
 
     useHtmlToPng2d(
         ref,
         {
-            pixelRatio: exportQuality.scaleFactor,
-            doTrimToBoundingBox: false
+            pixelRatio:
+                exportQuality.scaleFactor,
+
+            doTrimToBoundingBox:false
         }
     );
 
     return (
         <div className="canvas-wrapper">
+
             <div className="canvas-container">
+
                 <div
-                    className={
-                        `canvas canvas-${mode}-mode`
-                    }
-                    id="canvas"
+                    className={`canvas canvas-${mode}-mode`}
                     style={{
-                        width: scene.width,
-                        height: scene.height,
-                        // aspectRatio: `${scene.width}/${scene.height}`
-                        transform:`scale(${scale})`,
-                        transformOrigin:"center"
+                        width:previewWidth,
+                        height:previewHeight
                     }}
                 >
+
                     <div
+                        ref={ref}
                         className="canvas-render-surface"
                         style={{
-                            width: scene.width,
-                            height: scene.height
+                            width:scene.width,
+                            height:scene.height,
+                            transform:`scale(${scale})`,
+                            transformOrigin:"top left"
                         }}
-                        ref={ref}
                     >
+
                         <Canvas2D
                             scene={scene}
                         />
+
                     </div>
+
                 </div>
 
                 <CanvasControls />
+
                 <AnimationControls />
+
                 <DownloadControls />
+
             </div>
+
         </div>
     );
 }
--- src/components/Canvas/Canvas2D.tsx
+++ src/components/Canvas/Canvas2D.tsx
@@ -1,24 +1,50 @@
-import { createRenderContext } from "../../features/animation/engine/renderer/createRenderContext";
+import {
+    createRenderContext
+} from "../../features/animation/engine/renderer/createRenderContext";
+
 import SceneRenderer from "../../features/animation/engine/scene/SceneRenderer";
-import type { SceneData } from "../../features/animation/engine/scene/sceneTypes";
-import { useAnimationStore } from "../../features/animation/store/animationStore";
+
+import type {
+    SceneData
+} from "../../features/animation/engine/scene/sceneTypes";
+
+import {
+    useAnimationStore
+} from "../../features/animation/store/animationStore";
 
 
 type Canvas2DProps = {
     scene:SceneData;
+
+    width:number;
+    height:number;
 };
 
 
 export default function Canvas2D({
-    scene
+    scene,
+    width,
+    height
 }:Canvas2DProps){
-    const frame = useAnimationStore(state => state.currentFrame);
+
+    const frame =
+        useAnimationStore(
+            state => state.currentFrame
+        );
+
     const context =
         createRenderContext({
+
             frame,
-            fps: scene.fps,
-            width: scene.width,
-            height: scene.height
+
+            fps:scene.fps,
+
+            sceneWidth:scene.width,
+            sceneHeight:scene.height,
+
+            renderWidth:width,
+            renderHeight:height
+
         });
 
     return (
